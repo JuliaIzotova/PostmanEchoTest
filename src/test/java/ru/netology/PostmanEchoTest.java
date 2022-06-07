154a2918a5d7eee34884b1799943c1c50c2d3f6b
JuliaIzotova] <yuizotova15@gmail.com>
2022-06-07 22:57:21 +0300

    fix the problem

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -15,7 +15,7 @@ public class PostmanEchoTest {
                 .when()
                 .post("/post")
                 .then()
-                .statusCode(401)
+                .statusCode(200)
                 .body("data", equalTo("приветики на тесте"))
         ;
     }
